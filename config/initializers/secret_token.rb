328f4df5cc11da2914053862f7a6cb6c4ad1307b
Marc John Boanas <marcjohnboanas@gmail.com>
2014-03-22 18:12:17 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccountHero::Application.config.secret_key_base = 'ad7c3a5dcbdfc885ea7aa05c9cf136e3c0e4c46a8d8e1b0c5a6fa58f0bffc23f517c03006f1caabf5bba4111ab151f91892e31be5d79420e3172ad4be1d8e5ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AccountHero::Application.config.session_store :cookie_store, key: '_AccountHero_session'
